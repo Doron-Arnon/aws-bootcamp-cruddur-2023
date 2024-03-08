232f99bc84a78d5bdb35dca58e83827edfb9cc12
Doron-Arnon <doronar123+doron_github@gmail.com>
2024-03-08 19:30:35 +0000

    remove app.

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -88,7 +88,7 @@ cors = CORS(
 rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
 #@app.before_first_request
 #def init_rollbar():
-def app.app_context():
+def app_context():
     """init rollbar module"""
     rollbar.init(
         # access token
