1726d867b742865db0a37ef69c4304f2a04ec475
Doron-Arnon <doronar123+doron_github@gmail.com>
2024-06-15 11:59:38 +0000

    kill all connections script

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,7 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('cruddur_yes_name', 'doronar123+cruddur_yes@gmail.com' ,'cruddur_yes_username' ,'MOCK'),
-  ('Doron Arnon', 'doronar123+cruddur@gmail.com','DoronArnon' ,'MOCK');
-  #Run SQL
+  ('Doron Arnon', 'doronar123+cruddur@gmail.com','DoronArnon' ,'MOCK'),
   ('Londo Mollari','lmollari@centari.com','londo','Mock');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
