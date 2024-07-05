69ccd469344d34bc96ff64bbfd662c55fab5e644
Doron-Arnon <doronar123+doron_github@gmail.com>
2024-07-05 12:05:07 +0000

    before definding a lambda function

--- thumbling-serverless-cdk/bin/thumbling-serverless-cdk.ts
+++ thumbling-serverless-cdk/bin/thumbling-serverless-cdk.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { ThumbingServerlessCdkStack } from '../lib/thumbing-serverless-cdk-stack';
+import { ThumblingServerlessCdkStack} from '../lib/thumbling-serverless-cdk-stack';
 
 const app = new cdk.App();
-new ThumbingServerlessCdkStack(app, 'ThumbingServerlessCdkStack', {
+new ThumblingServerlessCdkStack(app, 'ThumblingServerlessCdkStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- thumbling-serverless-cdk/test/thumbling-serverless-cdk.test.ts
+++ thumbling-serverless-cdk/test/thumbling-serverless-cdk.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as ThumbingServerlessCdk from '../lib/thumbing-serverless-cdk-stack';
+// import * as ThumblingServerlessCdk from '../lib/thumbling-serverless-cdk-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/thumbing-serverless-cdk-stack.ts
+// example resource in lib/thumbling-serverless-cdk-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new ThumbingServerlessCdk.ThumbingServerlessCdkStack(app, 'MyTestStack');
+//   const stack = new ThumblingServerlessCdk.ThumblingServerlessCdkStack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
