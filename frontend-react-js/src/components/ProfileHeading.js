23beaa7f5a1fe728dc24cab0888d01ddde27c1ae
Doron-Arnon <doronar123+doron_github@gmail.com>
2024-07-28 15:52:13 +0000

    added banner and avatar

---
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -0,0 +1,29 @@
+import './ProfileHeading.css';
+import EditProfileButton from '../components/EditProfileButton';
+
+export default function ProfileHeading(props) {
+
+    const backgroundImage = 'url("https://s3.eu-north-1.amazonaws.com/assets.da-cruddur.com/banners/banner.jpg")';
+    const styles = {
+        backgroundImage: backgroundImage,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    }
+
+    return (
+    <div className='activity_feed_heading profile_heading'>
+    <div className='title'>{props.profile.display_name}</div>
+    <div className="cruds_count">{props.profile.cruds_count} Cruds</div>
+    <div class="banner" style={styles}>
+        <div className="avatar">
+            <img src="https://assets.cruddur.com/avatars/data.jpg"></img>
+        </div>
+    </div>
+    
+    <div className="display_name">{props.displey_name}</div>
+    <div className="handle">@{props.handle}</div>
+
+    <EditProfileButton setPopped={props.SetPopped}  />
+  </div>
+  );
+}
