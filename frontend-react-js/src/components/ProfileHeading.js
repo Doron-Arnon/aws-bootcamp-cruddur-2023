90d4a387142ab18cea3400764f08ea64d11937d5
Doron-Arnon <doronar123+doron_github@gmail.com>
2024-08-10 17:32:19 +0000

    added editform

--- frontend-react-js/src/components/ProfileHeading.js
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -25,7 +25,7 @@ export default function ProfileHeading(props) {
                 <div className="display_name">{props.profile.displey_name}</div>
                 <div className="handle">@{props.profile.handle}</div>
             </div>   
-            <EditProfileButton setPopped={props.SetPopped}  />
+            <EditProfileButton setPopped={props.setPopped}  />
         </div>
     </div>
   );
